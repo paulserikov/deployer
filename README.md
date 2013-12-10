a447608a4db6f860e251b97a902961e3b6bf1a90
Pavel Serikov <pavel.p.serikov@gmail.com>
2013-12-10 17:21:42 +0400

    Add compatibility from apache2 2.0.41 to 2.3.11 versions (NameVirtualHost and Include/IncludeOptional directives

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Simple site deployer on localhost
 Просто положите в корень проекта папку deploy и файлик deploy.sh<br>
 Что делает deploy.sh? Создает все симлинки и загружает конфигурацию виртуалхоста apache2,
 подмонтируя проект на домен <project_name>.local<br>
+
+В скрипте используется сервер apache2, редактор medit, браузер firefox, а также следующие утилиты linux:<br>
+<ul>
+<li>basename</li>
+<li>cd</li>
+<li>cp</li>
+<li>chmod</li>
+<li>cat</li>
+<li>echo</li>
+<li>grep</li>
+<li>ln</li>
+<li>ls</li>
+<li>pwd</li>
+<li>rm</li>
+<li>sed</li>
+<li>sudo</li>
+</ul>
--- godeploy.sh
+++ godeploy.sh
@@ -7,15 +7,18 @@
 PROJECT_PATH=$(pwd)
 echo "Project path is $PROJECT_PATH"
 A=$(basename $PWD)
-# may also be as A=`basename $PWD`
 cd ..
 sudo chmod -R 755 $A
+# may also be as A=`basename $PWD`
 echo "Project name is $A"
-echo "Reading Apache2.conf..."
-echo "Please edit this file if it is not OK"
+apache2 -v
+echo "Reading main Apache2 config (/etc/apache2/apache2.conf)... Please edit this file if it is not OK"
+echo "prior to 2.3.11, NameVirtualHost is required to instruct the server that a particular IP address and port combination was usable as a name-based virtual host"
+echo "Directive IncludeOptional (Available in 2.3.6 and later) or Include (2.0.41 and later) must be uncomment (especially sites-enabled and mods-enabled). Grep Include in apache2.conf..."
 echo "========================================="
-cat /etc/apache2/apache2.conf | grep IncludeOptional
+cat /etc/apache2/apache2.conf | grep Include
 echo "========================================="
+echo "Manually add NameVirtualHost and IncludeOptional directive if it's needed"
 echo "Would u like to edit this file? y/n"
 read  ans
 if [ $ans = "y" ]
@@ -28,7 +31,7 @@ echo "========================================="
 ls -la
 echo "========================================="
 cd $PROJECT_PATH
-echo "Preparing virtualhost config file..."
+echo "Preparing virtualhost config file, which name is $A.local.conf..."
 REPLACEMENT_KEY="project"
 NEW_VALUE=$A
 PATH_TO_R_CONFIG_FILE="$PWD/deploy/project.local.conf"
@@ -38,6 +41,7 @@ echo "========================================="
 sudo cp $PROJECT_PATH/deploy/$A.local.conf /etc/apache2/sites-available/$A.local.conf
 cd /etc/apache2/sites-available
 sudo chmod a+rx $A.local.conf
+echo "Reading /etc/apache2/sites-available catalog..."
 echo "========================================="
 ls -la
 echo "========================================="
@@ -46,8 +50,9 @@ sudo service apache2 reload
 sudo service apache2 stop
 cd $PROJECT_PATH
 sudo cat /etc/hosts > tmphost
-sudo echo "127.0.0.1 $A.local" >> tmphost
+echo "127.0.0.1 $A.local" >> tmphost
 sudo cp tmphost /etc/hosts
 rm tmphost
 sudo service apache2 start
+echo "If u get an error 'Could not reliably determine the server's fully qualified domain name, using 127.0.1.1 for ServerName' add to first string of /etc/hosts 127.0.0.1 localhost.localdomain localhost my-desktop"
 firefox $A.local
